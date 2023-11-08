2ac6a1c638bb0276a7f554e663f65a00948ac119
tankwell <idan.tankel@weizmann.ac.il>
2023-11-08 19:06:06 +0200

    change timelimit for jobs

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #SBATCH -J SEED_Bench_%a
-#SBATCH --time=05:00:00
+#SBATCH --time=08:00:00
 #SBATCH --mem=48G
 /usr/bin/env /home/idanta/anaconda3/envs/lama/bin/python SEED-Bench/BLIP2_eval.py --question_type_id $SLURM_ARRAY_TASK_ID

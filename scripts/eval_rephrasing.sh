9e58ffc4d542ee1a38ab5063a31ad249875c3659
tankwell <idan.tankel@weizmann.ac.il>
2023-12-12 09:27:52 +0200

    bugfix 🐛

--- scripts/eval_rephrasing.sh
+++ scripts/eval_rephrasing.sh
@@ -4,4 +4,4 @@
 #SBATCH --output=slurm_logs/%j.out
 #SBATCH --time=06:00:00
 #SBATCH --mem=48G
-/usr/bin/env /home/idanta/anaconda3/envs/lama/bin/python ../SEED-Bench/BLIP2_eval_reprhasing.py --question_type_id $SLURM_ARRAY_TASK_ID
+/usr/bin/env /home/idanta/anaconda3/envs/lama/bin/python SEED-Bench/BLIP2_eval_reprhasing.py --question_type_id $SLURM_ARRAY_TASK_ID
